7a09436cc356e43e2d1f0ed239012917bf412de1
DawidSwiatkowski <dawid.swiatkowski@gmail.com>
2022-01-27 16:01:57 +0100

    Add create_in file

---
+++ create_bin.bat
@@ -0,0 +1,6 @@
+rd /S /Q .\bin
+mkdir .\bin
+cd bin
+xcopy .\..\birthdays.db
+xcopy .\..\..\build-Birthdays-Desktop_Qt_6_2_2_MSVC2019_64bit-Release\release\Birthdays.exe
+c:\Qt\6.2.2\msvc2019_64\bin\windeployqt.exe Birthdays.exe
